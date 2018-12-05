427b119e80a0db4e0501137ef14c868eded6aab7
clouds0061 <15871650177m0@sina.cn>
2018-12-05 09:55:04 +0800

    0.0.2 画出网格，画出第一个L形状方块并让其匀速下落
    疏影横斜水清浅，暗香浮动月黄昏。

--- miniBlock/pages/mini_block/block.js
+++ miniBlock/pages/mini_block/block.js
@@ -8,18 +8,36 @@ Page({
 
   },
 
+  btnStart: function (){
+    var time = 0;
+    var my_carvas = wx.createCanvasContext('canvas', this) //1.创建carvas实例对象，方便后续用。
+    drawBlock(my_carvas, 0)
+    start(time);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
   },
-
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    var my_carvas = wx.createCanvasContext('canvas', this) //1.创建carvas实例对象，方便后续用。
+    my_carvas.setStrokeStyle('orange') //设置边框颜色。
+    for(var i = 0;i<15;i++){
+      for(var j = 0 ;j<27;j++){
+        //my_carvas.strokeRect(0, 0, 20*i, 20*i)
+        var left = 20 * i;
+        var top = 20*j;
+        var right = 20;
+        var bottom = 20;
+        my_carvas.strokeRect(left, top, right, bottom)
+      }
+    }
+    my_carvas.draw()   //将之前在绘图上下文中的描述（路径、变形、样式）画到 canvas 中。
   },
 
   /**
@@ -64,3 +82,30 @@ Page({
 
   }
 })
+
+function start(time){
+  var my_carvas = wx.createCanvasContext('canvas', this) //1.创建carvas实例对象，方便后续用
+  setTimeout(function () {
+    drawBlock(my_carvas, time)
+  }, 1000)
+}
+
+function drawBlock(my_carvas,time){
+  my_carvas.setStrokeStyle('orange') //设置边框颜色。
+  for (var i = 0; i < 15; i++) {
+    for (var j = 0; j < 27; j++) {
+      //my_carvas.strokeRect(0, 0, 20*i, 20*i)
+      var left = 20 * i;
+      var top = 20 * j;
+      var right = 20;
+      var bottom = 20;
+      my_carvas.strokeRect(left, top, right, bottom)
+    }
+  }
+  my_carvas.setFillStyle('red')
+  my_carvas.fillRect(40, time*20, 80, 20);
+  my_carvas.fillRect(40, (time+1)*20, 20, 20);
+  my_carvas.draw();
+  time++;
+  if(time<26)start(time)
+}
